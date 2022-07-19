d694d27bd9dc0a5d7f8089c8dc33789eb6b3446d
bearjin <kwj5030@gmail.com>
2022-07-19 13:44:24 +0900

    UI 추가

--- todolist/src/app.js
+++ todolist/src/app.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const res = await axios.get("/api/users");
-  console.log(res);
+// // document.addEventListener("DOMContentLoaded", async () => {
+// //   const res = await axios.get("/api/users");
 
-  document.body.innerHTML = res.data
-    .map(user => {
-      return `<div>${user.id}: ${user.name}</div>`;
-    })
-    .join("---");
-});
+// //   document.body.innerHTML = res.data
+// //     .map(user => {
+// //       return `<div>${user.id}: ${user.name}</div>`;
+// //     })
+// //     .join("---");
+// // });
+
+import "./scss/app.scss";
