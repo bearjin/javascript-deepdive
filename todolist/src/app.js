0b42b88e4bbff5ccb896256767d817f342c6f223
bearjin <kwj5030@gmail.com>
2022-08-16 11:50:04 +0900

    [Update] 로직 변경

--- todolist/src/app.js
+++ todolist/src/app.js
@@ -3,37 +3,7 @@ import "./scss/app.scss";
 class TodoContainer {
   constructor() {
     this.todoItems = [];
-    this.todoCompleteItems = [];
     this.id = 0;
-    this.$todoListDo = document.querySelector("#todoListDo");
-    this.$todoListComplete = document.querySelector("#todoListComplete");
-
-    this.initArea(this.$todoListDo, "do");
-    this.initArea(this.$todoListComplete, "complete");
-  }
-
-  initArea(target, type) {
-    const $badge = document.createElement("span");
-    const $list = document.createElement("ul");
-    const $btn = document.createElement("button");
-
-    $badge.className = "todoList__badge";
-    $badge.classList.add(`todoList__badge--${type}`);
-    $badge.textContent =
-      type === "do" ? "😆 할일" : type === "complete" ? "😎 완료" : null;
-
-    $list.className = "todoList__list";
-
-    $btn.className = "todoList__add";
-    $btn.textContent = "새로 만들기";
-    $btn.addEventListener("click", () => {
-      this.createNewItem();
-      this.makeList(target);
-    });
-
-    target.appendChild($badge);
-    target.appendChild($list);
-    target.appendChild($btn);
   }
 
   createNewItem() {
@@ -48,9 +18,6 @@ class TodoContainer {
 
   deleteItem(id) {
     this.todoItems = this.todoItems.filter(item => item.id !== id);
-    this.todoCompleteItems = this.todoCompleteItems.filter(
-      item => item.id !== id
-    );
   }
 
   changeDoneItem(id, text) {
@@ -66,40 +33,55 @@ class TodoContainer {
     this.todoItems.forEach(item => {
       if (item.id === id) {
         item.isComplete = isComplete;
-
-        if (isComplete) this.todoCompleteItems.push(item);
-        this.todoItems = this.todoItems.filter(
-          item => item.isComplete === false
-        );
-      }
-    });
-
-    this.todoCompleteItems.forEach(item => {
-      if (item.id === id) {
-        item.isComplete = isComplete;
-        if (!isComplete) this.todoItems.push(item);
-        this.todoCompleteItems = this.todoCompleteItems.filter(
-          item => item.isComplete === true
-        );
       }
     });
   }
+}
 
-  makeList() {
-    const $doList = this.$todoListDo.querySelector(".todoList__list");
-    const $completeList = this.$todoListComplete.querySelector(
-      ".todoList__list"
-    );
+class TodoArea {
+  constructor(area, type) {
+    this.container = new TodoContainer();
+    this.area = document.querySelector(area);
+    this.type = type;
+  }
 
-    $doList.innerHTML = "";
-    $completeList.innerHTML = "";
+  initArea() {
+    const $badge = document.createElement("span");
+    const $list = document.createElement("ul");
+    const $btn = document.createElement("button");
 
-    this.todoItems.forEach(({ id, text, isDone, isComplete }) => {
-      $doList.appendChild(this.makeItem(id, text, isDone, isComplete));
+    $badge.className = "todoList__badge";
+    $badge.classList.add(`todoList__badge--${this.type}`);
+    $badge.textContent =
+      this.type === "do"
+        ? "😆 할일"
+        : this.type === "complete"
+        ? "😎 완료"
+        : null;
+
+    $list.className = "todoList__list";
+
+    $btn.className = "todoList__add";
+    $btn.textContent = "새로 만들기";
+    $btn.addEventListener("click", () => {
+      this.container.createNewItem();
+      this.makeList(this.container.todoItems);
     });
 
-    this.todoCompleteItems.forEach(({ id, text, isDone, isComplete }) => {
-      $completeList.appendChild(this.makeItem(id, text, isDone, isComplete));
+    this.area.appendChild($badge);
+    this.area.appendChild($list);
+    this.area.appendChild($btn);
+  }
+
+  makeList(data) {
+    const $list = this.area.querySelector(".todoList__list");
+
+    $list.innerHTML = "";
+
+    console.log(data);
+
+    data.forEach(({ id, text, isDone, isComplete }) => {
+      $list.appendChild(this.makeItem(id, text, isDone, isComplete));
     });
   }
 
@@ -120,9 +102,8 @@ class TodoContainer {
       $input.checked = isComplete;
       $input.addEventListener("change", event => {
         const isComplete = event.target.checked;
-        this.changeCompleteItem(id, isComplete);
-        this.makeList(this.$todoListDo);
-        this.makeList(this.$todoListComplete);
+        this.container.changeCompleteItem(id, isComplete);
+        this.makeList(this.container.todoItems);
       });
 
       $label.setAttribute("for", `todo${id}`);
@@ -134,8 +115,8 @@ class TodoContainer {
       $input.placeholder = "텍스트를 입력하세요";
       $input.addEventListener("change", event => {
         const text = event.target.value;
-        this.changeDoneItem(id, text);
-        this.makeList(this.$todoListDo);
+        this.container.changeDoneItem(id, text);
+        this.makeList(this.container.todoItems);
       });
     }
 
@@ -143,8 +124,8 @@ class TodoContainer {
     $button.className = "todoList__delete";
     $button.textContent = "Delete";
     $button.addEventListener("click", () => {
-      this.deleteItem(id);
-      this.makeList(this.$todoListDo);
+      this.container.deleteItem(id);
+      this.makeList(this.container.todoItems);
     });
 
     $item.appendChild($input);
@@ -155,4 +136,14 @@ class TodoContainer {
   }
 }
 
-new TodoContainer();
+class TodoSetting {
+  constructor() {
+    this.doArea = new TodoArea("#todoListDo", "do");
+    this.completeArea = new TodoArea("#todoListComplete", "complete");
+
+    this.doArea.initArea();
+    this.completeArea.initArea();
+  }
+}
+
+new TodoSetting();
